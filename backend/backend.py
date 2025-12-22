d68a3dc8a8badd4f70ba09ef93c610a37d830560
Ayden Garza <aydenjgarza@gmail.com>
2025-12-21 20:42:44 -0600

    Cleaning

--- backend/backend.py
+++ backend/backend.py
@@ -3,10 +3,3 @@ from requests.credential_request import CredentialRequest
 from requests.credential_request import CredentialResponse
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {"data": "what Yeah"}
-
-@app.get('/login', response_model=CredentialResponse)
-async def login_attempt(request: CredentialRequest):
-    return {"user_id": 32,"username": request.username,"auth_token": "kioj3oiv3i4jij1fojoif", "premium_user": False}
