bdcdd03697779502bf13c98e1ec5457e4d1ede62
Ayden Garza <aydenjgarza@gmail.com>
2025-12-24 03:09:42 -0600

    Added registration functionality, needs testing

--- backend/security/auth.py
+++ backend/security/auth.py
@@ -1,10 +1,16 @@
 from pydantic import BaseModel
+from urllib.parse import quote
 from starlette import status
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi import HTTPException
 from backend.models.login_request import LoginRequest
+from backend.models.register_request import RegisterRequest
+from backend.database.db import get_db_connection
+from typing import Annotated
+from supabase import Client
 import os
 
 router = APIRouter(prefix='/auth', tags=['auth'])
@@ -12,7 +18,7 @@ router = APIRouter(prefix='/auth', tags=['auth'])
 SECRET_KEY = os.environ.get('AUTH_HASH_KEY')
 ALGORITHM = os.environ.get('SECRET_ALGORITHM')
 
-crypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+crypt_context = CryptContext(schemes=['bcrypt_sha256'], deprecated='auto')
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 class Token(BaseModel):
@@ -28,5 +34,22 @@ async def login(request: LoginRequest):
     return {"login": "request received"}
 
 @router.post("/register", status_code=status.HTTP_200_OK)
-async def register_user():
-    pass
+async def register_user(request: RegisterRequest, db: Annotated[Client, Depends(get_db_connection)]):
+    username = request.username
+    email = request.email
+    quote_username = quote(request.username)
+    quote_email = quote(request.email)
+    existing_accounts_response = db.table('users').select('username,email').or_(f"username.eq.{username},email.eq.{email}").limit(1).execute()
+    existing_accounts = existing_accounts_response.data
+
+    if existing_accounts:
+        existing_username = existing_accounts[0]['username']
+        if existing_username == username:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Account with that username already exists")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Account with that email already exists")
+
+    hashed_pass = crypt_context.hash(request.password)
+    account_creation_res = (db.table('users').insert({'username': username, 'password': hashed_pass, 'email': email}).execute())
+    if not account_creation_res.data:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Account creation failed, try again later")
+    return {"REQUEST": "registration", "user_registered": username, "SUCESS": True} 
