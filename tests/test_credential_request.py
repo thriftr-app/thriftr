96548a87dd522235d04b9e8ef68551aa969de15c
Ayden Garza <aydenjgarza@gmail.com>
2025-12-24 19:58:10 -0600

    Added pytest configuration, improved email validating with pydantic, updated tests

--- tests/test_credential_request.py
+++ tests/test_credential_request.py
@@ -10,7 +10,7 @@ def test_no_at_sign_email():
     with pytest.raises(ValidationError):
         LoginRequest(email="dkowkdokd", password="jV8v89$jvIOaj8")
 
-@pytest.mark.parametrize("email", ["@gmail.com", "dd@gmail.com", "normal@google", "normal@normal.", "normal@n.c", "normal@cdcdc."])
+@pytest.mark.parametrize("email", ["@gmail.com", "normal@google", "normal@normal.", "normal@cdcdc."])
 def test_invalid_emails(email):
     with pytest.raises(ValidationError) as test_info:
         LoginRequest(email=email, password = "iV9%*FFf%jfi2f9")
